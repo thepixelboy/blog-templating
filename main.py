cf4f7496829c870b5cf65fd3cffef5a65feb9adc
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-03 09:50:52 +0200

    feat: make read link on each post points to corresponding blog post

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ def home():
     return render_template("index.html", posts=all_posts)
 
 
+@app.route("/blog/<int:post_id>")
+def post(post_id):
+    response = requests.get(blog_url)
+    all_posts = response.json()
+
+    for post in all_posts:
+        if post["id"] == post_id:
+            title = post["title"]
+            body = post["body"]
+    return render_template("post.html", title=title, body=body)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
