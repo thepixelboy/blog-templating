d8640d0f52d50456d6906acc80e045f6a3636711
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-03 08:45:49 +0200

    feat: render all the blog's titles and subtitles on the home page

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import requests
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
+blog_url = "https://api.npoint.io/4af156202f984d3464c3"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
